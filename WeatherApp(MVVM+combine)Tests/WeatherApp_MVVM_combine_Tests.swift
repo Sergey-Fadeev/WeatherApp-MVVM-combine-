7e0f0138b516201663bb0fe30d88e2e4a835eb6c
Sergey <seregahromoi@icloud.com>
2021-10-12 16:51:02 +0400

    Initial Commit

---
+++ WeatherApp(MVVM+combine)Tests/WeatherApp_MVVM_combine_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherApp_MVVM_combine_Tests.swift
+//  WeatherApp(MVVM+combine)Tests
+//
+//  Created by Sergey on 12.10.2021.
+//
+
+import XCTest
+@testable import WeatherApp_MVVM_combine_
+
+class WeatherApp_MVVM_combine_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
